b126389d8afe7c25ba0cd96803c21e0fb2c67129
Jonathan Shine <jonathanhshine@gmail.com>
2021-11-10 15:15:24 +0100

    added GroupLogin files which were forgotten on last commit

---
+++ app/screens/groupLogin/GroupLoginOrCreation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import CustomText from '../../components/general/CustomText'
+
+const GroupLoginOrCreation = () => {
+  return (
+    <View>
+      <CustomText
+        title="Group Sign In"
+        h1
+      />
+      
+    </View>
+  )
+}
+
+export default GroupLoginOrCreation
