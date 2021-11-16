87ae8bf2a31ba2d4895d3c384a2e20761b189f05
Jonathan Shine <jonathanhshine@gmail.com>
2021-11-16 13:46:20 +0100

    started adding form to Calculator for the user to add an expense to a group

--- app/screens/calculator/Calculator.js
+++ app/screens/calculator/Calculator.js
@@ -1,10 +1,73 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, ScrollView, StatusBar, Text, TextInput } from 'react-native'
+import { useForm, Controller } from 'react-hook-form';
+import CustomText from '../../components/general/CustomText'
+
+// * STYLES IMPORT
+import generalStyles from '../../styles/generalStyles';
 
 const Calculator = () => {
+  const { control, handleSubmit, formState: { errors } } = useForm();
+
   return (
-    <SafeAreaView>
-      <Text>Calculator Screen</Text>
+    <SafeAreaView style={generalStyles.AndroidSafeArea}>
+      <StatusBar barStyle="dark-content"/>
+      <ScrollView style={generalStyles.appContainer}>
+        <CustomText
+          title="Calculator"
+          h2
+        />
+        
+        <CustomText
+          title="Overview: "
+          h3
+        />
+        
+        <CustomText
+          title="Add Expense: "
+          h3
+        />
+
+      <Controller
+        control={control}
+        rules={{
+          required: true
+        }}
+        render={({ field: { onChange, value, onBlur }}) => (
+          <TextInput
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Title"
+            autoCapitalize="false"
+          />
+        )}
+        name="expenseTitle"
+        defaultValue=""
+      />
+      {errors.expenseTitle && <Text>Title is required.</Text>}
+      
+      <Controller
+        control={control}
+        rules={{
+          required: true
+        }}
+        render={({ field: { onChange, value, onBlur }}) => (
+          <TextInput
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Cost"
+            autoCapitalize="none"
+            keyboardType="numeric"
+          />
+        )}
+        name="expenseCost"
+        defaultValue=""
+      />
+      {errors.expenseCost && <Text>Cost is required.</Text>}
+
+      </ScrollView>
     </SafeAreaView>
   )
 }
