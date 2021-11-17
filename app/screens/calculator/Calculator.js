42a5d34453910363e27c60a9ecd098cf24d2e5c1
Jonathan Shine <jonathanhshine@gmail.com>
2021-11-17 11:03:36 +0100

    added DateTimePicker package and created inital Add Expense functionality in Calculator

--- app/screens/calculator/Calculator.js
+++ app/screens/calculator/Calculator.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import { SafeAreaView, ScrollView, StatusBar, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { Button, SafeAreaView, ScrollView, StatusBar, Text, TextInput, View } from 'react-native'
 import { useForm, Controller } from 'react-hook-form';
-import CustomText from '../../components/general/CustomText'
+import CustomText from '../../components/general/CustomText';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
 
 // * STYLES IMPORT
 import generalStyles from '../../styles/generalStyles';
 
 const Calculator = () => {
+  const [isPickerOpen, setIsPickerOpen] = useState(false);
+  const [date, setDate] = useState(new Date(Date.now()));
+
   const { control, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log({...data, expenseDate: date});
+  };
+
+  const datePickerHandler = (selectedDate) => {
+    if (Platform.OS === 'android') setIsPickerOpen(false)
+    setDate(selectedDate);
+  };
+
+  const showDatePicker = () => {
+    setIsPickerOpen(true);
+  };
 
   return (
     <SafeAreaView style={generalStyles.AndroidSafeArea}>
@@ -39,7 +57,7 @@ const Calculator = () => {
             onChangeText={onChange}
             value={value}
             placeholder="Title"
-            autoCapitalize="false"
+            autoCapitalize="none"
           />
         )}
         name="expenseTitle"
@@ -57,15 +75,49 @@ const Calculator = () => {
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
-            placeholder="Cost"
-            autoCapitalize="none"
             keyboardType="numeric"
+            placeholder="Cost"
           />
         )}
         name="expenseCost"
         defaultValue=""
       />
       {errors.expenseCost && <Text>Cost is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          required: false
+        }}
+        render={({ field: { onChange, value, onBlur }}) => (
+          <TextInput
+            onFocus={() => showDatePicker()}
+            onChangeText={onChange}
+            value={date.toLocaleDateString()}
+            placeholder="Date"
+          />
+        )}
+        name="expenseDate"
+        defaultValue=""
+      />
+
+      {/* take DateTimePicker out of the controller and use it to assign to the TextInput inside the form */}
+
+      {isPickerOpen && (
+              <View>
+                <DateTimePicker
+                  minimumDate={new Date(1901, 0, 1)}
+                  maximumDate={new Date()}
+                  value={new Date()}
+                  mode="date"
+                  display="default"
+                  onChange={(event, value) => datePickerHandler(value)}
+                />
+               </View>
+      )}
+
+      <Button title="Add Expense" onPress={handleSubmit(onSubmit)}/>
+
 
       </ScrollView>
     </SafeAreaView>
