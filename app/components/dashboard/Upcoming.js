5f6b9cf2f7e84c391c2e4fb5e8762ec395768c45
Tim Jungmann <tim-j11@web.de>
2021-11-29 14:41:18 +0100

    fix upcoming tasks and events to show them all

--- app/components/dashboard/Upcoming.js
+++ app/components/dashboard/Upcoming.js
@@ -8,7 +8,6 @@ import { useGroup } from '../../context/GroupContext';
 // * COMPONENTS IMPORT
 import CustomText from '../general/CustomText';
 import Badge from '../general/Badge';
-import { isToday } from '../general/Time';
 
 // * STYLES IMPORT
 import generalStyles from '../../styles/generalStyles';
@@ -16,8 +15,6 @@ import dashboardStyles from './dashboardStyles';
 
 const Upcoming = () => {
   const { groupData } = useGroup();
-  const [upcomingTasks, setUpcomingTasks] = useState([]);
-  const [upcomingEvents, setUpcomingEvents] = useState([]);
 
   const [upcoming1, setUpcoming1] = useState([]);
   const [upcoming2, setUpcoming2] = useState([]);
@@ -28,6 +25,10 @@ const Upcoming = () => {
   }, [groupData]);
 
   const printItems = () => {
+    let up1 = [];
+    let up2 = [];
+    let up3 = [];
+
     groupData.tasks.map((task, index)=>{
       const newTask = (
         <View key={`t-${index}`} style={dashboardStyles.rowFlex}>
@@ -36,9 +37,9 @@ const Upcoming = () => {
           <Badge title={"task"} task userColor={"red"} style={{flex:1}}/>
         </View>
       )
-      if(moment(task.date).format("DD") == moment().add(1,'days').format("DD")) setUpcoming1([...upcoming1, newTask])
-      if(moment(task.date).format("DD") == moment().add(2,'days').format("DD")) setUpcoming2([...upcoming2, newTask])
-      if(moment(task.date).format("DD") == moment().add(3,'days').format("DD")) setUpcoming3([...upcoming3, newTask])
+      if(moment(task.date).format("DD") == moment().add(1,'days').format("DD")) up1.push(newTask);
+      if(moment(task.date).format("DD") == moment().add(2,'days').format("DD")) up2.push(newTask);
+      if(moment(task.date).format("DD") == moment().add(3,'days').format("DD")) up3.push(newTask);
     })
     groupData.events.map((event, index)=>{
       const eventHours = moment(event.date).format("HH");
@@ -50,10 +51,14 @@ const Upcoming = () => {
           <Badge title={`${eventHours}:${eventMinutes}`} event style={{flex:1}}/>
         </View>
       )
-      if(moment(event.date).format("DD") == moment().add(1,'days').format("DD")) setUpcoming1([...upcoming1, newEvent])
-      if(moment(event.date).format("DD") == moment().add(2,'days').format("DD")) setUpcoming2([...upcoming2, newEvent])
-      if(moment(event.date).format("DD") == moment().add(3,'days').format("DD")) setUpcoming3([...upcoming3, newEvent])
+      if(moment(event.date).format("DD") == moment().add(1,'days').format("DD")) up1.push(newEvent);
+      if(moment(event.date).format("DD") == moment().add(2,'days').format("DD")) up2.push(newEvent);
+      if(moment(event.date).format("DD") == moment().add(3,'days').format("DD")) up3.push(newEvent);
     })
+    
+    setUpcoming1(up1);
+    setUpcoming2(up2);
+    setUpcoming3(up3);
   }
 
   return (
